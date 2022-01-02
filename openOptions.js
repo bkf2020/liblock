8f0fe1bf0d39a2bb5c2499e631b03cecb37d0391
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2022-01-01 17:46:52 -0800

    Update button in the popup when a user blocks websites

--- openOptions.js
+++ openOptions.js
@@ -6,3 +6,11 @@ document.addEventListener('DOMContentLoaded', function () {
 	document.getElementById('optionsButton')
 		.addEventListener('click', openOptions);
 });
+
+chrome.storage.sync.get(['blocking'], function(result) {
+	if(result.blocking) {
+		document.getElementById("optionsButton").innerText = "view websites you are blocking and time left";
+	} else {
+		document.getElementById("optionsButton").innerText = "change blocked websites and start blocking";
+	}
+});
